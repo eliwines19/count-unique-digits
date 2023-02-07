1abdd9bdc69a184c96f44bf019c51242a21affb3
Elijah Wines <wines.elijah@gmail.com>
2023-02-06 20:37:33 -0500

    finished challenge

--- CountUniqueDigits.java
+++ CountUniqueDigits.java
@@ -1,8 +1,64 @@
-
 public class CountUniqueDigits {
 
     static void printUniqueDigits(int[][] arr) {
-        System.out.println(arr);
+
+        // iterate through input array
+        for (int i = 0; i < arr.length; i++) {
+            
+            // establish start and end of range
+            int start = arr[i][0];
+            int end = arr[i][1];
+            int count = 0;
+
+            // iterate through the range of numbers
+            for (int j = start; j <= end; j++) {
+
+                // if number is unique increment count
+                if (isUnique(j) == true) {
+                    count++;
+                }
+
+            }
+
+            // print total count of unique numbers
+            System.out.println(count);
+
+        }
+
+    }
+
+    static boolean isUnique(int num) {
+        // initialize count as 0
+        int count = 0;
+
+        // compare each digit with others
+        // starts comparing rightmost digit with leftmost digit
+        while (num > 0) {
+
+            // get rightmost digit
+            int digit = num % 10;
+            // remove the rightmost digit
+            num = num / 10;
+            // assing rightmost digit to temp variable
+            int temp = num;
+
+            // check if rightmost digit matches leftmost digits
+            while (temp > 0) {
+
+                // if rightmost digit matches with any leftmost digits increase count by 1
+                if (temp % 10 == digit) {
+                    count = 1;
+                    break;
+                }
+
+                // remove rightmost digit from temp and get updated temp value
+                temp = temp / 10;
+
+            }
+        }
+
+        if (count == 0) {return true;}
+        return false;
     }
 
     public static void main(String args[]) {
