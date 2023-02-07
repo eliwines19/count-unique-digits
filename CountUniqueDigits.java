3d2b15bcd523a96fe046757a508d0606a07cd2a0
Elijah Wines <wines.elijah@gmail.com>
2023-02-06 19:02:11 -0500

    created file structure

---
+++ CountUniqueDigits.java
@@ -0,0 +1,12 @@
+
+public class CountUniqueDigits {
+
+    static void printUniqueDigits(int[][] arr) {
+        System.out.println(arr);
+    }
+
+    public static void main(String args[]) {
+        int arr[][] = {{1, 10}, {1, 20}, {30, 45}};
+        printUniqueDigits(arr);
+    }
+}
